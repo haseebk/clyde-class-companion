f3dfbe5d8ae9dd6f85d0defdc75978628105bb45
Haseeb <haseebadilkhan@gmail.com>
2020-12-11 00:33:22 -0700

    reformatting code

--- commands/deadline.js
+++ commands/deadline.js
@@ -1,10 +1,10 @@
 module.exports = {
-    name: 'deadline',
-    description: 'Add an upcoming deadline for a class.',
-    args: true, 
-    usage: '<course name + course num> <deadline type> <deadline date DD/MM>',
-    guildOnly: true,
-    execute(message, args){
-        message.channel.send("Add a deadline for a class!");
-    }
-}
+  name: "deadline",
+  description: "Add an upcoming deadline for a class.",
+  args: true,
+  usage: "<course name + course num> <deadline type> <deadline date DD/MM>",
+  guildOnly: true,
+  execute(message, args) {
+    message.channel.send("Add a deadline for a class!");
+  },
+};
--- commands/help.js
+++ commands/help.js
@@ -1,33 +1,36 @@
-const {prefix} = require('../config.json');
+const { prefix } = require("../config.json");
 
 module.exports = {
-    name: 'help',
-    description: 'Show a list of commands or usage info about a command.',
-    usage: '<command name>',
-    guildOnly: true,
-    execute(message, args){
-        const data = [];
-        const {commands} = message.client;
-        if (!args.length){
-            data.push('List of all things Clyde can do:');
-            data.push(commands.map(command => command.name).join(', '));
-            data.push(`Use \`${prefix}help <command name>\` for more detailed information of a specific command.`);
+  name: "help",
+  description: "Show a list of commands or usage info about a command.",
+  usage: "<command name>",
+  guildOnly: true,
+  execute(message, args) {
+    const data = [];
+    const { commands } = message.client;
+    if (!args.length) {
+      data.push("List of all things Clyde can do:");
+      data.push(commands.map((command) => command.name).join(", "));
+      data.push(
+        `Use \`${prefix}help <command name>\` for more detailed information of a specific command.`
+      );
 
-            return message.channel.send(data, {split: true})
-
-        };
-
-        const name = args[0].toLowerCase();
-        const command = commands.get(name);
-
-        if (!command){
-            return message.channel.send(`That command does not exist! Please look at the list of available comamnds using \`${prefix}${command.name}\``);
-        }
-        data.push(`**${command.name}**: ${command.description}`);
-        // if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** \`${prefix}${command.name} ${command.usage}\``);
-        
-
-        message.channel.send(data, {split: true});
+      return message.channel.send(data, { split: true });
     }
-}
+
+    const name = args[0].toLowerCase();
+    const command = commands.get(name);
+
+    if (!command) {
+      return message.channel.send(
+        `That command does not exist! Please look at the list of available comamnds using \`${prefix}${command.name}\``
+      );
+    }
+    data.push(`**${command.name}**: ${command.description}`);
+    // if (command.description) data.push(`**Description:** ${command.description}`);
+    if (command.usage)
+      data.push(`**Usage:** \`${prefix}${command.name} ${command.usage}\``);
+
+    message.channel.send(data, { split: true });
+  },
+};
